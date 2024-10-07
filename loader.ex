125126f2dabd7fba0d5762df19f5840b5b8034fd
Victor Lyashenko <kenuay007@gmail.com>
2024-10-07 19:57:22 +0300

    Go! Go! Points

--- loader.ex
+++ loader.ex
@@ -1,7 +1,7 @@
 defmodule Loader do
   defstruct memory_size: 0, program: [%{}]
 
-  @memory_size 36
+  @memory_size 64
 
   def load(file_path) do
     %Loader{
@@ -19,12 +19,31 @@ defmodule Loader do
   defp load_program_from_file(file_path) do
     case File.read(file_path) do
       {:ok, file_contents} ->
-        instructions =
+        instructions_with_labels =
           file_contents
           |> String.split("\n")
           |> Enum.map(&parse_instruction/1)
+          |> Enum.with_index()
 
-        instructions
+        labels =
+          instructions_with_labels
+          |> Enum.reduce(%{}, fn {instruction, index}, labels ->
+            case instruction do
+              {:label, label} ->
+                Map.put(labels, label, index)
+
+              _ ->
+                labels
+            end
+          end)
+
+        instructions_without_labels =
+          instructions_with_labels
+          |> Enum.map(fn {instruction, index} ->
+            replace_labels(instruction, index, labels)
+          end)
+
+        instructions_without_labels
 
       {:error, reason} ->
         raise "Error loading program from file #{file_path}: #{reason}"
@@ -33,16 +52,48 @@ defmodule Loader do
 
   defp parse_instruction(line) do
     case String.split(line) do
-      ["IF", addr1, addr2] -> {:if, String.to_integer(addr1), String.to_integer(addr2)}
-      ["SI", addr1, addr2] -> {:store_init, String.to_integer(addr1), String.to_integer(addr2)}
-      ["SFR", reg1, reg2] -> {:store_from_reg, parse_reg(reg1), parse_reg(reg2)}
-      ["LI", reg1, addr1] -> {:load_init, parse_reg(reg1), String.to_integer(addr1)}
-      ["LFR", reg1, reg2] -> {:load_from_reg, parse_reg(reg1), parse_reg(reg2)}
-      ["ADD", reg1, reg2] -> {:add, parse_reg(reg1), parse_reg(reg2)}
-      ["SUB", reg1, reg2] -> {:sub, parse_reg(reg1), parse_reg(reg2)}
-      ["GOTO", addr1] -> {:goto, String.to_integer(addr1)}
-      ["HALT"] -> {:halt}
-      _ -> raise "Unknown instruction: #{line}}"
+      ["!" <> label] ->
+        {:label, label}
+
+      ["IF", addr1, addr2] ->
+        case {addr1, addr2} do
+          {"!" <> label1, "!" <> label2} -> {:if, label1, label2}
+          _ -> {:if, String.to_integer(addr1), String.to_integer(addr2)}
+        end
+
+      ["SI", addr1, addr2] ->
+        {:store_init, String.to_integer(addr1), String.to_integer(addr2)}
+
+      ["SFR", reg1, reg2] ->
+        {:store_from_reg, parse_reg(reg1), parse_reg(reg2)}
+
+      ["LI", reg1, addr1] ->
+        {:load_init, parse_reg(reg1), String.to_integer(addr1)}
+
+      ["LFR", reg1, reg2] ->
+        {:load_from_reg, parse_reg(reg1), parse_reg(reg2)}
+
+      ["ADD", reg1, reg2] ->
+        {:add, parse_reg(reg1), parse_reg(reg2)}
+
+      ["SUB", reg1, reg2] ->
+        {:sub, parse_reg(reg1), parse_reg(reg2)}
+
+      ["GOTO", addr1] ->
+        case addr1 do
+          "!" <> label ->
+            {:goto, label}
+
+          _ ->
+            {addr1_as_integer, ""} = Integer.parse(addr1)
+            {:goto, addr1_as_integer}
+        end
+
+      ["HALT"] ->
+        {:halt}
+
+      _ ->
+        raise "Unknown instruction: #{line}}"
     end
   end
 
@@ -56,6 +107,22 @@ defmodule Loader do
       "IFR" -> :ifreg
       "IR" -> :ir
       "PC" -> :pc
+    end
+  end
+
+  def replace_labels(instruction, index, labels) do
+    case instruction do
+      {:if, label1, label2} when is_binary(label1) and is_binary(label2) ->
+        {:if, Map.get(labels, label1), Map.get(labels, label2)}
+
+      {:goto, label} when is_binary(label) ->
+        {:goto, Map.get(labels, label)}
+
+      {:label, label} when is_binary(label) ->
+        {:goto, index + 1}
+
+      _ ->
+        instruction
     end
   end
 end
